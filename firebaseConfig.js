66168d6f826dd17fb55ac64ea2d9e41f6a882895
Blessen-VS <134699741+Blessen-VS@users.noreply.github.com>
2024-12-11 00:29:04 +0530

    Update firebaseConfig.js

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,10 +1,24 @@
 const admin = require('firebase-admin');
-const serviceAccount = require('./firebase-service-account.json'); // Ensure correct path
+const path = require('path');
+require('dotenv').config();
+
+const serviceAccountPath = path.resolve(__dirname, './paths/firebase-service-account.json');
 
 // Initialize Firebase Admin SDK only once
 if (!admin.apps.length) {
   admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount),
+    credential: admin.credential.cert({
+      type: process.env.FIREBASE_TYPE,
+      project_id: process.env.FIREBASE_PROJECT_ID,
+      private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
+      private_key: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+      client_email: process.env.FIREBASE_CLIENT_EMAIL,
+      client_id: process.env.FIREBASE_CLIENT_ID,
+      auth_uri: process.env.FIREBASE_AUTH_URI,
+      token_uri: process.env.FIREBASE_TOKEN_URI,
+      auth_provider_x509_cert_url: process.env.FIREBASE_AUTH_PROVIDER_CERT_URL,
+      client_x509_cert_url: process.env.FIREBASE_CLIENT_CERT_URL,
+    }),
   });
 }
 
